188cd5c00ee4d0aeaa4b17aefbba1fc1d61d4533
Aswanthkrishnanm <vysakhraj2000@gmail.com>
2024-01-31 21:16:20 +0530

    added adding employee

---
+++ src/app/addemployee/addemployee.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employees/models/employee.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-addemployee',
+  standalone: true,
+  imports: [],
+  templateUrl: './addemployee.component.html',
+  styleUrl: './addemployee.component.css'
+})
+export class AddemployeeComponent {
+
+  addEmployeeRequest :Employee={
+    id:'',
+    name:'',
+    email:'',
+    phone:0,
+    salary:0,
+    department:''
+  }
+}
